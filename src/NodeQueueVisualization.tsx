d40b057f3e11b35b715a81718bcf0ead76d9d0f2
Timm Haucke <timmh@haucke.xyz>
2019-11-17 18:05:51 +0100

    mark in and out of the node queue

--- src/NodeQueueVisualization.tsx
+++ src/NodeQueueVisualization.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Node } from "./CytoscapeGraph";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const NodeQueueVisualization = (
   { nodes }: { nodes: Node[] } = { nodes: [] }
@@ -8,15 +10,38 @@ const NodeQueueVisualization = (
     <div className="node-queue-visualization">
       <span className="node-queue-visualization__title">Queue q = </span>
       <div className="node-queue-visualization__elements">
-        {nodes.map(node => (
-          <div key={node.getId()} className="node-queue-visualization__element">
-            <div
-              className={`node-queue-visualization__label node-queue-visualization__label--type-${node.getType()}`}
-            >
-              {node.getLabel()}
-            </div>
+        <div className="node-queue-visualization__elements-front">
+          <div className="node-queue-visualization__element">
+            <FontAwesomeIcon
+              size="sm"
+              icon={faArrowLeft}
+              style={{ margin: "0.2em" }}
+            />
+            out
           </div>
-        ))}
+          {nodes.map(node => (
+            <div
+              key={node.getId()}
+              className="node-queue-visualization__element"
+            >
+              <div
+                className={`node-queue-visualization__label node-queue-visualization__label--type-${node.getType()}`}
+              >
+                {node.getLabel()}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="node-queue-visualization__elements-back">
+          <div className="node-queue-visualization__element">
+            <FontAwesomeIcon
+              size="sm"
+              icon={faArrowLeft}
+              style={{ margin: "0.2em" }}
+            />
+            in
+          </div>
+        </div>
       </div>
     </div>
   );
